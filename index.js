53d778b810957199fadb9f6449d5c619ffb568fd
Likhith Reddy Rechintala <54302844+likhithreddy@users.noreply.github.com>
2025-04-02 22:55:24 -0400

    testing Netlify URL

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import "dotenv/config";
 const app = express()
 app.use(cors({
     credentials: true,
-    origin: process.env.NETLIFY_URL || "http://localhost:5173",
+    origin: [process.env.NETLIFY_URL, "http://localhost:5173"]
 }));
 const sessionOptions = {
     secret: process.env.SESSION_SECRET || "kambaz",
