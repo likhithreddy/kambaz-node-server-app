885b833d8937c944de21bfd63158053cb0bb8c0e
Likhith Reddy Rechintala <54302844+likhithreddy@users.noreply.github.com>
2025-04-17 16:47:52 -0400

    implemented until 6.4.4

--- Kambaz/Courses/routes.js
+++ Kambaz/Courses/routes.js
@@ -1,6 +1,7 @@
 import * as dao from "./dao.js";
 import * as modulesDao from "../Modules/dao.js";
 import * as assignmentsDao from "../Assignments/dao.js";
+import * as enrollmentsDao from "../Enrollments/dao.js";
 
 export default function CourseRoutes(app) {
   app.get("/api/courses", async (req, res) => {
@@ -10,6 +11,11 @@ export default function CourseRoutes(app) {
 
   app.post("/api/courses", async (req, res) => {
     const course = await dao.createCourse(req.body);
+    const currentUser = req.session["currentUser"];
+    if (currentUser) {
+      await enrollmentsDao.enrollUserInCourse(currentUser._id, course._id);
+    }
+
     res.json(course);
   });
 
@@ -57,4 +63,11 @@ export default function CourseRoutes(app) {
     const assignments = await assignmentsDao.findAssignmentsForCourse(courseId);
     res.json(assignments);
   });
+
+  const findUsersForCourse = async (req, res) => {
+    const { cid } = req.params;
+    const users = await enrollmentsDao.findUsersForCourse(cid);
+    res.json(users);
+  };
+  app.get("/api/courses/:cid/users", findUsersForCourse);
 }
--- Kambaz/Enrollments/dao.js
+++ Kambaz/Enrollments/dao.js
@@ -1,23 +1,3 @@
-// import Database from "../Database/index.js";
-// import { v4 as uuidv4 } from "uuid";
-
-// export function getEnrollmentsByUser(userId) {
-//   const { enrollments } = Database;
-//   return enrollments.filter((enrollment) => enrollment.user === userId);
-// }
-
-// export function enrollUserInCourse(courseId, userId) {
-//     const { enrollments } = Database;
-//     enrollments.push({ _id: uuidv4(), user: userId, course: courseId });
-// }
-
-// export function unenrollUserFromCourse(courseId, userId) {
-//     const { enrollments } = Database;
-//     Database.enrollments = enrollments.filter(
-//         (enrollment) => !(enrollment.user === userId && enrollment.course === courseId)
-//     );
-// }
-
 import model from "./model.js";
 export async function findCoursesForUser(userId) {
   const enrollments = await model.find({ user: userId }).populate("course");
