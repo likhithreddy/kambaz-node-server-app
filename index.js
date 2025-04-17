235e23215a7c59665c9acb5e0c3108c536da6350
Likhith Reddy Rechintala <54302844+likhithreddy@users.noreply.github.com>
2025-04-17 00:39:45 -0400

    implemented until 6.6.2.3

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
-import express from 'express';
+import express from "express";
 import Hello from "./Hello.js";
 import cors from "cors";
 import Lab5 from "./Lab5/index.js";
 import UserRoutes from "./Kambaz/Users/routes.js";
 import CourseRoutes from "./Kambaz/Courses/routes.js";
 import ModuleRoutes from "./Kambaz/Modules/routes.js";
-import AssignmentRoutes from './Kambaz/Assignments/routes.js';
-import EnrollmentRoutes from './Kambaz/Enrollments/routes.js';
+import AssignmentRoutes from "./Kambaz/Assignments/routes.js";
+import EnrollmentRoutes from "./Kambaz/Enrollments/routes.js";
 import session from "express-session";
+import mongoose from "mongoose";
+const CONNECTION_STRING =
+  process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz";
+mongoose.connect(CONNECTION_STRING);
 import "dotenv/config";
 
-const app = express()
-app.use(cors({
+const app = express();
+app.use(
+  cors({
     credentials: true,
     origin: process.env.NETLIFY_URL || "http://localhost:5173",
-}));
+  })
+);
 
 const sessionOptions = {
-    secret: process.env.SESSION_SECRET || "kambaz",
-    resave: false,
-    saveUninitialized: false,
+  secret: process.env.SESSION_SECRET || "kambaz",
+  resave: false,
+  saveUninitialized: false,
 };
 if (process.env.NODE_ENV !== "development") {
-    sessionOptions.proxy = true;
-    sessionOptions.cookie = {
-        sameSite: "none",
-        secure: true,
-        domain: process.env.NODE_SERVER_DOMAIN,
-    };
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.NODE_SERVER_DOMAIN,
+  };
 }
 app.use(session(sessionOptions));
 
@@ -37,6 +43,6 @@ CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
 EnrollmentRoutes(app);
-Lab5(app)
-Hello(app)
-app.listen(process.env.PORT || 4000)
+Lab5(app);
+Hello(app);
+app.listen(process.env.PORT || 4000);
